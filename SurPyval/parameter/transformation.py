10a1cdf5ca9b7cf2d18ee933d84635ce6752ec1d
Jake Coltman <jakecoltman@sky.com>
2018-01-11 08:29:36 +0000

    Add documentation and type hinting

--- SurPyval/parameter/transformation.py
+++ SurPyval/parameter/transformation.py
@@ -1,16 +1,31 @@
+from typing import Callable, Dict, Any
+
 class Transformation(object):
     """
-        Defines a transformation from (data, parameter) -> value
-        Using transformed versions of variables simplifies down stream code
+        Allows the definition of composite "parameters" to add to the parameter dictionary
 
-        Example use case:
+        Useful for simplifying the code of other nodes by pre-baking calculations
+        e.g. in a regression model, the linear predictor could be a transformation.
 
-            Transforming beta into the linear predictor in exponential regression
+        Parameters
+        ----------
+        f_transformation: NodeTree
+                          Function to apply to parameters and data_dict to produce new parameter
+                          Must take as parameters (data_dict, parameter_dict) and return an array-like
+        name: str
+                  Name of the resulting parameter as should appear in parameter_dict
     """
 
-    def __init__(self, f, new_name):
-        self.f = f
-        self.new_name = new_name
+    def __init__(self, f_transformation: Callable[[Dict[str, Any], Dict[str, Any]], Any], name):
+        self.f_transformation = f_transformation
+        self.name = name
 
     def transform(self, data_dict, parameter_dict):
-        return {self.new_name: self.f(data_dict, parameter_dict)}
+        """
+        Apply the transformation function to data and parameters
+
+        Returns
+        -------
+        array-like
+        """
+        return {self.name: self.f_transformation(data_dict, parameter_dict)}
